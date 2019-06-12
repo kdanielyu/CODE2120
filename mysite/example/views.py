88a77c922381e86c637a2c22ea0e83261232d899
kdanielyu <39843977+kdanielyu@users.noreply.github.com>
2019-06-12 18:43:45 +1000

    643

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -9,7 +9,7 @@ from .models import *
 def example_get(request, var_a):
 	try:
 		returnob = {
-		"print(var_a)",
+		"data": "%s: %s" %(var_a),
 		}
 		return JsonResponse(returnob)
 	except Exception as e:
