54483c86c332b68cb0184d20a4949c5610a6f554
kdanielyu <39843977+kdanielyu@users.noreply.github.com>
2019-06-12 18:13:36 +1000

    613

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'e/', include('example.urls')),
-    path(r'wombo/', include('wombat.urls')),
+    #path('admin/', admin.site.urls),
+   # path(r'e/', include('example.urls')),
+    #path(r'wombo/', include('wombat.urls')),
     #####WHATEVER
 ]
