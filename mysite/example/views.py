d2444674c111c906ec10985edf09b78b66c45759
kdanielyu <39843977+kdanielyu@users.noreply.github.com>
2019-06-12 23:24:58 +1000

    Update views.py

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
-import BeautifulSoup
+import BeautifulSoup4
 import requests
 from .models import *
 
